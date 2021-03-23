118c784b056c8e1c38aed9047c0de7aec3046ad4
Wan Bachtiar <sindbach@gmail.com>
2021-03-23 16:23:11 +1100

    Updated helper script to use GCR image

--- get-started.sh
+++ get-started.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 MONGODB_URI=${1}
+DOCKER_IMAGE=ghcr.io/mongodb-developer/get-started-cxx
+
 if [ -z ${MONGODB_URI} ]
 then
     read -p "MONGODB URI (Required): " MONGODB_URI
-fi 
+fi
 
-DRIVER_VERSION=${2:-3.6.2}
 echo "Executing ... "
 docker run --rm -e MONGODB_URI=${MONGODB_URI} \
     -v "$(pwd)":/workspace \
-    -w /workspace/cxx start-cxx \
-    "c++ --std=c++11 ./getstarted.cpp -o getstarted -I/usr/local/include/mongocxx/v_noabi -I/usr/local/include/bsoncxx/v_noabi -L/usr/local/lib -lmongocxx -lbsoncxx; \
+    -w /workspace/cxx ${DOCKER_IMAGE} \
+    "c++ --std=c++11 ./getstarted.cpp -o getstarted -I/usr/local/include/mongocxx/v_noabi -I/usr/local/include/bsoncxx/v_noabi -L/usr/local/lib64 -lmongocxx -lbsoncxx; \
     ./getstarted"
